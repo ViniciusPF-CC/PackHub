084117e6e64ae562f9a2d3fc8f9dc9cdce2c5b2b
ViniciusPF-CC <58273512+ViniciusPF-CC@users.noreply.github.com>
2023-10-16 14:23:59 -0300

    Firt

---
+++ src/main/java/distribuicaoembalagem/DistribuicaoEmbalagem.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package distribuicaoembalagem;
+
+import view.FrLogin;
+
+/**
+ *
+ * @author vinic
+ */
+public class DistribuicaoEmbalagem {
+
+    public static void main(String[] args) {
+        FrLogin login = new FrLogin();
+        login.setVisible(true);
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/model/User.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Data;
+
+/**
+ *
+ * @author Otavio
+ */
+@Entity
+@Data
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+    private String email;
+    private String cpf;
+    private String phone;
+    private String typePositions;
+    
+    public User(){
+        id= -1;
+        nome = "";
+        email = "";
+        cpf = "";
+        phone = "";
+        typePositions = "user";
+    }
+    public User(Integer id, String nome, String email, String cpf, String phone, String typePositions){
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.cpf =  cpf;
+        this.phone = phone;
+        this.typePositions = typePositions;
+    }
+    
+}
