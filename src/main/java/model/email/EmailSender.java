e0732434d2d4a9bec32a837dbb6ebf76d767033e
ViniciusPF-CC <58273512+ViniciusPF-CC@users.noreply.github.com>
2023-12-11 08:40:48 -0300

    Email

---
+++ src/main/java/model/email/EmailSender.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.email;
+
+import jakarta.mail.Authenticator;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import java.util.Properties;
+
+/**
+ *
+ * @author vinic
+ */
+public class EmailSender {
+
+    public boolean sendEmail(String to, String subject, String text) {
+        boolean flag = false;
+        String from = "FROMEMAIL";
+
+        //logic
+        //smtp properties
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", true);
+        properties.put("mail.smtp.starttls.enable", true);
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+
+        String username = "vinicius.fialho.p@gmail.com";
+        String password = "sjve vict dqkx gryf";
+
+        //session
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setFrom(new InternetAddress(from));
+            message.setSubject(subject);
+            message.setContent(text, "text/html");
+            Transport.send(message);
+            flag = true;
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
+        return flag;
+    }
+
+}
